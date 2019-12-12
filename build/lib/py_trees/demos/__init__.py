73e078a503fd55348aa8d2371014d9f4052e2016
Pilar de la Cruz <pilar.de-la-cruz@uibk.ac.at>
2019-12-12 10:56:03 +0100

    added attentional_trees demo in __init__.py

---
+++ build/lib/py_trees/demos/__init__.py
@@ -0,0 +1,27 @@
+#
+# License: BSD
+#   https://raw.githubusercontent.com/splintered-reality/py_trees/devel/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
+
+"""
+This package contains py_trees demo script code.
+"""
+##############################################################################
+# Imports
+##############################################################################
+
+from . import action  # noqa
+from . import blackboard  # noqa
+from . import blackboard_namespaces  # noqa
+from . import blackboard_remappings  # noqa
+from . import context_switching  # noqa
+from . import dot_graphs  # noqa
+from . import lifecycle  # noqa
+from . import selector  # noqa
+from . import sequence  # noqa
+from . import stewardship  # noqa
+from . import test_expand_tree_json # noqa
+from . import test_automated_planning # noqa
